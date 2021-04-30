5a059788b3a9994afb49398d7c8dde0f29f95a0c
ayush goel <ayush4898@gmail.com>
2021-04-30 19:01:54 +0530

    added navbarand pages

--- frontend/src/PageRoutes.js
+++ frontend/src/PageRoutes.js
@@ -2,29 +2,56 @@ import React from 'react';
 // import './PageRoutes.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './components/Pages';
-import About from './components/Pages/about';
-import Events from './components/Pages/events';
-import AnnualReport from './components/Pages/annual';
-import Teams from './components/Pages/team';
-import Blogs from './components/Pages/blogs';
-import SignUp from './components/Pages/signup';
 
-function PageRoutes() {
-return (
-	<Router>
-	<Navbar />
-	<Switch>
-		<Route path='/' exact component={Home} />
-		<Route path='/about' component={About} />
-		<Route path='/events' component={Events} />
-		<Route path='/annual' component={AnnualReport} />
-		<Route path='/team' component={Teams} />
-		<Route path='/blogs' component={Blogs} />
-		<Route path='/sign-up' component={SignUp} />
-	</Switch>
-	</Router>
-);
+
+// for student 
+import Profile from './components/Pages/student/profile';
+import RegistrationForm from './components/Pages/student/registrationForm';
+import Status from './components/Pages/student/status';
+import UpdateProfile from './components/Pages/student/updateProfile';
+import Signout from './components/Pages/student/signout';
+ // for teacher
+import ProfileTeacher from './components/Pages/teacher/profileTeacher';
+import AddNewUser from './components/Pages/teacher/addNewUser';
+import PendingRegistration from './components/Pages/teacher/pendingRegistration';
+import SearchUser from './components/Pages/teacher/searchUser';
+import Update from './components/Pages/teacher/update';
+import SignoutTeacher from './components/Pages/teacher/signout';
+
+
+
+function PageRoutes(props) {  //props : userId isStudent
+	if (props.isStudent)   //show registration form by default
+	{
+		return (
+			<Router>
+			<Navbar isStudent={props.isStudent} userId={props.userId}/>
+			<Switch>
+				<Route path='/' exact component={RegistrationForm} />
+				<Route path='/profile' component={Profile} />
+				<Route path='/status' component={Status} />
+				<Route path='/updateProfile' component={UpdateProfile} />
+				<Route path='/signout' component={Signout} />
+			</Switch>
+			</Router>
+		);
+	}
+	else {
+		return (
+			<Router>
+			<Navbar isStudent={props.isStudent} userId={props.userId}/>
+			<Switch>
+				<Route path='/' exact component={PendingRegistration} />
+				<Route path='/profile' component={ProfileTeacher} />
+				<Route path='/addNewUser' component={AddNewUser} />
+				<Route path='/search' component={SearchUser} />
+				<Route path='/update' component={Update} />
+				<Route path='/signout' component={SignoutTeacher} />
+			</Switch>
+			</Router>
+		);
+	}
+
 }
 
 export default PageRoutes;
--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -8,40 +8,70 @@ NavBtn,
 NavBtnLink,
 } from './NavbarElements';
 
-const Navbar = () => {
-return (
-	<>
-	<Nav>
-		<Bars />
+const Navbar = (props) => {
+	if (props.isStudent)
+	{
+		return (
+			<>
+			<Nav>
+				<Bars />
+				<NavMenu>
+				<NavLink to='/' activeStyle>
+					Registration form
+				</NavLink>
+				<NavLink to='/profile' activeStyle>
+					Profile
+				</NavLink>
+				<NavLink to='/status' activeStyle>
+					Status
+				</NavLink>
+				<NavLink to='/updateProfile' activeStyle>
+					Update Profile
+				</NavLink>
+				{/* Second Nav */}
+				{/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+				</NavMenu>
+				<NavBtn>
+				<NavBtnLink to='/signout'>LogOut</NavBtnLink>
+				</NavBtn>
+			</Nav>
+			</>
+		);
+				
+	}
+	else
+	{
 
-		<NavMenu>
-		<NavLink to='/about' activeStyle>
-			About
-		</NavLink>
-		<NavLink to='/events' activeStyle>
-			Events
-		</NavLink>
-		<NavLink to='/annual' activeStyle>
-			Annual Report
-		</NavLink>
-		<NavLink to='/team' activeStyle>
-			Teams
-		</NavLink>
-		<NavLink to='/blogs' activeStyle>
-			Blogs
-		</NavLink>
-		<NavLink to='/sign-up' activeStyle>
-			Sign Up
-		</NavLink>
-		{/* Second Nav */}
-		{/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
-		</NavMenu>
-		<NavBtn>
-		<NavBtnLink to='/signin'>Sign In</NavBtnLink>
-		</NavBtn>
-	</Nav>
-	</>
-);
+		return (
+			<>
+				<Nav>
+					<Bars />
+					<NavMenu>
+					<NavLink to='/' activeStyle>
+							Registration Forms
+				    </NavLink>
+					<NavLink to='/profile' activeStyle>
+							Profile
+					</NavLink>
+					<NavLink to='/addNewUser' activeStyle>
+							Add User
+					</NavLink>
+					<NavLink to='/update' activeStyle>
+							Update Profile
+					</NavLink>
+					<NavLink to='/search' activeStyle>
+						 Search
+					</NavLink>
+						{/* Second Nav */}
+						{/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+					</NavMenu>
+					<NavBtn>
+						<NavBtnLink to='/signout'>LogOut</NavBtnLink>
+					</NavBtn>
+				</Nav>
+			</>
+		);
+	}
 };
 
 export default Navbar;
